8471715c4038e08890f6aefba9d4fece15a9ee73
0xFluffyBeard <smooth.lion8476@fastmail.com>
2022-06-03 02:53:25 +0200

    Install hardhat-contract-sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-contract-sizer";
 
 dotenv.config();
 
@@ -37,6 +38,11 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: false,
+    disambiguatePaths: false,
   },
 };
 
