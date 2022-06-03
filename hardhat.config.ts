2f71e7e6edba52e7c8cae5e59201522d45808d1c
0xFluffyBeard <smooth.lion8476@fastmail.com>
2022-06-03 03:27:04 +0200

    Initial configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,25 +24,40 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    hardhat: {
+      forking: {
+          enabled: process.env.FORK_URL != null,
+          url: process.env.FORK_URL || "",
+          blockNumber: parseInt(process.env.FORK_BLOCK as string),
+        },
+      gas: "auto",
+    },
+    ftmTestnet: {
+      url: process.env.FTM_TESTNET_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: process.env.REPORT_GAS as any as boolean || false,
+    coinmarketcap: process.env.CMC_API_KEY,
     currency: "USD",
-  },
+    token: "FTM",
+    gasPriceApi: "https://api.ftmscan.com/api?module=proxy&action=eth_gasPrice",
+},
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      opera: process.env.FTMSCAN_API_KEY,
+      ftmTestnet: process.env.FTMSCAN_API_KEY,
+    },
   },
   contractSizer: {
     alphaSort: false,
     runOnCompile: false,
     disambiguatePaths: false,
+    except: ['^.*Mock$'],
   },
 };
 
